a0685fa756327ee92096bf5c18ed944ee0b62721
mingz <305603665@qq.com>
2016-05-04 17:11:03 +0800

    proxy

--- proxy_spider/setup.py
+++ proxy_spider/setup.py
@@ -6,11 +6,11 @@ from setuptools import setup, find_packages
 setup(name='scrapy-proxy_spider',
       entry_points={
           'scrapy.commands': [
-              'crawlall=proxy_spider.commands:crawlall',
-              'dump_to_valid=proxy_spider.commands:dump_to_valid',
-              'valid_jd=proxy_spider.commands:valid_jd',
-              'dump_to_jd=proxy_spider.commands:dump_to_jd',
-              'valid_valid=proxy_spider.commands:valid_valid',
+              'mc_crawlall=proxy_spider.commands:crawlall',
+              'mc_dump_to_valid=proxy_spider.commands:dump_to_valid',
+              'mc_valid_jd=proxy_spider.commands:valid_jd',
+              'mc_dump_to_jd=proxy_spider.commands:dump_to_jd',
+              'mc_valid_valid=proxy_spider.commands:valid_valid',
           ],
       },
       )
