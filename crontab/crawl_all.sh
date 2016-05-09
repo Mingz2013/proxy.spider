756b6bc25ec0619d358d00d4ffc1ebe28c62300f
mingz <305603665@qq.com>
2016-05-09 14:43:05 +0800

    proxy

--- crontab/crawl_all.sh
+++ crontab/crawl_all.sh
@@ -3,4 +3,4 @@ cd /home/apps/proxy_spider
 dt=$(date "+%Y-%m-%d")
 path_to_log="log/""$dt""_crawl_all.log"
 echo $path_to_log
-scrapy mc_crawlall -s LOG_FILE=$path_to_log &
+scrapy mc_crawl_all -s LOG_FILE=$path_to_log &
