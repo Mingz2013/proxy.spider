977fc44085403a35dc5b2449a8b3ca15671e4d68
mingz <305603665@qq.com>
2016-10-10 17:09:43 +0800

    init

--- proxy_spider/setup.py
+++ proxy_spider/setup.py
@@ -7,9 +7,6 @@ setup(name='scrapy-proxy_spider',
       entry_points={
           'scrapy.commands': [
               'mc_crawl_all=proxy_spider.commands:mc_crawl_all',
-              'mc_dump_to_valid=proxy_spider.commands:mc_dump_to_valid',
-              'mc_valid_valid=proxy_spider.commands:mc_valid_valid',
-              'mc_valid_drop=proxy_spider.commands:mc_valid_drop',
               'mc_crawl_proxy_api=proxy_spider.commands:mc_crawl_proxy_api'
           ],
       },
