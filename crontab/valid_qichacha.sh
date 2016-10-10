9a6c7e4052623b960a19bc3f21e1cc9ea08caaad
mingz <305603665@qq.com>
2016-10-10 16:56:10 +0800

    init

--- crontab/valid_qichacha.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd /home/apps/proxy_spider
-dt=$(date "+%Y-%m-%d")
-path_to_log="log/""$dt""_valid_qichacha.log"
-echo $path_to_log
-scrapy mc_valid_qichacha -s LOG_FILE=$path_to_log &
--- shell/mongo_boot.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-mongod -dbpath data/db
