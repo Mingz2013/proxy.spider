504f4ee1697fc41e0235d7410a78789183ec53ec
mingz <305603665@qq.com>
2016-10-17 14:48:52 +0800

    init

--- helper/get_proxy.py
+++ helper/get_proxy.py
@@ -19,6 +19,6 @@ class GetProxy(object):
         except Exception, e:
             self._proxy_cur = self._db.get_proxy_items()
             if self._proxy_cur.count() == 0:
-                logging.info("proxy is empty, sleep 30 * 60s")
-                time.sleep(30 * 60)
+                logging.info("proxy is empty, sleep 10 * 60s")
+                time.sleep(10 * 60)
             return self.get_proxy()
